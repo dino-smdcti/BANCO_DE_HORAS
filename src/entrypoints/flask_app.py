a36f43b7ebf2a01d24cfdfb695ba9de34b21f77f
shanokthethird <xxshanok3xx@gmail.com>
2026-05-04 14:05:25 -0300

    feat: implement favicon and specific route to resolve 404 errors

--- src/entrypoints/flask_app.py
+++ src/entrypoints/flask_app.py
@@ -267,6 +267,11 @@ def magic_link_login(token):
     flash("Usuário não encontrado.", "danger")
     return redirect(url_for("login"))
 
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, "static"),
+                               "favicon.ico", mimetype="image/vnd.microsoft.icon")
+
 @app.route("/")
 def index():
     return render_template("index.html")
