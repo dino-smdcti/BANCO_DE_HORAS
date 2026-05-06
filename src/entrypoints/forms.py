bc1e85f397dc5ff15136c025bb841415f4f91816
shanokthethird <xxshanok3xx@gmail.com>
2026-05-06 17:25:53 -0300

    feat: make cpf/matricula optional, add matricula mask, pre-fill secretaria, and auto-capitalize fields

--- src/entrypoints/forms.py
+++ src/entrypoints/forms.py
@@ -14,8 +14,8 @@ class RegisterForm(FlaskForm):
 
 class ProfileForm(FlaskForm):
     full_name = StringField("Nome Completo", validators=[DataRequired()])
-    registration_number = StringField("Matrícula", validators=[DataRequired()])
-    cpf = StringField("CPF", validators=[DataRequired(), Length(min=11, max=14)])
+    registration_number = StringField("Matrícula")
+    cpf = StringField("CPF")
     department = StringField("Departamento", validators=[DataRequired()])
     position = StringField("Cargo", validators=[DataRequired()])
     secretariat = StringField("Secretaria", validators=[DataRequired()])
